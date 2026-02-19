8a68bb3e113ca291389584b1f86acba75e633458
ashitomi-ryui <ryui-ashitomi-k25f@stu.kbc.ac.jp>
2026-02-19 10:08:52 +0900

    Player.hの保存

--- TeamA/Sources/GameObjects/Player/Player.h
+++ TeamA/Sources/GameObjects/Player/Player.h
@@ -14,9 +14,33 @@ public:
 	~Player();
 
 public:
+
+	/// <summary>
+	/// ‰Šú‰»ˆ—
+	/// </summary>
 	void Initialize() override;
+
+	/// <summary>
+	/// XVˆ—
+	/// </summary>
 	void UpDate() override;
+
+	/// <summary>
+	/// •`‰æˆ—
+	/// </summary>
 	void Draw() const override;
+
+	/// <summary>
+	/// I—¹ˆ—
+	/// </summary>
 	void Finalize() override;
+	
+public:
+
+	/// <summary>
+	/// ”»’èˆ—
+	/// </summary>
+	/// <param name="other"></param>
+	void OnHitCollision(ObjectBase& other) override;
 };
 
