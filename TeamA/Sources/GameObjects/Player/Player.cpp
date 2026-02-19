8cb3318d4321d4e58bc3cdb9deab64e576581e28
ashitomi-ryui <ryui-ashitomi-k25f@stu.kbc.ac.jp>
2026-02-19 09:58:09 +0900

    Playerの関数の作成

--- TeamA/Sources/GameObjects/Player/Player.cpp
+++ TeamA/Sources/GameObjects/Player/Player.cpp
@@ -1,1 +1,46 @@
 #include "Player.h"
+
+#include <DxLib.h>
+
+Player::Player()
+{
+
+}
+
+Player::Player::~Player()
+{
+
+}
+
+void Player::Initialize()
+{
+	m_stamina = 100;
+	m_animTime = 0.0f;
+	m_animCount = 0;
+	m_moveSpeed = {};
+}
+
+void Player::UpDate()
+{
+	m_animTime += 0.01f;
+	if (m_animTime > 1.0f)
+	{
+		m_animTime = 0.0f;
+		m_animCount += 1;
+	}
+}
+
+void Player::Draw() const
+{
+	DrawCircle(m_location.x, m_location.y, 10, GetColor(0, 255, 255), TRUE);
+}
+
+void Player::Finalize()
+{
+
+}
+
+void Player::OnHitCollision(ObjectBase& other)
+{
+
+}
--- TeamA/Sources/GameObjects/Player/Player.h
+++ TeamA/Sources/GameObjects/Player/Player.h
@@ -1,5 +1,22 @@
 #pragma once
-class Player
-{
+#include "../ObjectBase.h"
+#include "../../Utilitys/Vector2D.h"
+
+class Player : public ObjectBase
+{private:
+	int m_stamina;
+	float m_animTime;
+	int m_animCount;
+	Vector2D m_moveSpeed;
+
+public:
+	Player();
+	~Player();
+
+public:
+	void Initialize() override;
+	void UpDate() override;
+	void Draw() const override;
+	void Finalize() override;
 };
 
