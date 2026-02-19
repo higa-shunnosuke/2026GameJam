0f023a3bb1889193cdd97c24a0a0d516e74cc954
ナカモト <tasuku-nakamoto-k24h@stu.kbc.ac.jp>
2026-02-19 14:11:10 +0900

    当たり判定の削除

--- TeamA/Sources/GameObjects/Collision/Collision.cpp
+++ TeamA/Sources/GameObjects/Collision/Collision.cpp
@@ -12,13 +12,5 @@ const e_ObjectType Collision::GetObjectType() const
 
 const bool Collision::IsCircleColliding(const Vector2D& centerA, const Vector2D& centerB, const float& radiusB) const
 {
-    // 2“_ŠÔ‚Ì‹——£‚Ì2æ‚ğ‹‚ß‚é
-    Vector2D diff = centerA - centerB;
-    float lendth = diff.SqrLength();
 
-    // ”¼Œa‚ğ‘«‚µ‚½2æ‚ğ‹‚ß‚é
-    float total_radius = (m_radius + radiusB)*(m_radius + radiusB);
-
-    // ’·‚³‚ğ”äŠr
-    return lendth < total_radius;
 }
