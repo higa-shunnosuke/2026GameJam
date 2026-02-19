9f297065d69ce84f1b727dc1c32862f2d5882a67
souya-teruya-k25f-ship-it <souya-teruya-k25f@stu.kbc.ac.jp>
2026-02-19 09:57:10 +0900

    Jewelの関数作成

--- TeamA/Sources/GameObjects/Jewel/Jewel.cpp
+++ TeamA/Sources/GameObjects/Jewel/Jewel.cpp
@@ -1,1 +1,44 @@
 #include "Jewel.h"
+
+#include <DxLib.h>
+
+Jewel::Jewel()
+{
+
+}
+
+Jewel::~Jewel()
+{
+
+}
+
+void Jewel::Initialize()
+{
+	m_animeTime = 0.0f;
+	m_animeCount = 0;
+}
+
+void Jewel::UpDate()
+{
+	m_animeTime += 0.01f;
+	if (m_animeTime > 1.0f)
+	{
+		m_animeTime = 0.0f;
+		m_animeCount += 1;
+	}
+}
+
+void Jewel::Draw() const
+{
+	DrawCircle(m_location.x, m_location.y, 10, GetColor(255, 0, 0), TRUE);
+}
+
+void Jewel::Finalize()
+{
+
+}
+
+void Jewel::OnHitCollision(ObjectBase& other)
+{
+
+}
--- TeamA/Sources/GameObjects/Jewel/Jewel.h
+++ TeamA/Sources/GameObjects/Jewel/Jewel.h
@@ -1,5 +1,44 @@
 #pragma once
-class Jewel
+#include "../ObjectBase.h"
+
+class Jewel : public ObjectBase
 {
+private:
+	float m_animeTime;
+	int m_animeCount;
+
+public:
+	Jewel();
+	~Jewel();
+
+public:
+
+	/// <summary>
+	/// ‰Šú‰»ˆ—
+	/// </summary>
+	void Initialize() override;
+
+	/// <summary>
+	/// XVˆ—
+	/// </summary>
+	void UpDate() override;
+
+	/// <summary>
+	/// •`‰æˆ—
+	/// </summary>
+	void Draw() const override;
+
+	/// <summary>
+	/// I—¹ˆ—
+	/// </summary>
+	void Finalize() override;
+
+public:
+
+	/// <summary>
+	/// ”»’èˆ—
+	/// </summary>
+	/// <param name="other"></param>
+	void OnHitCollision(ObjectBase& other) override;
 };
 
