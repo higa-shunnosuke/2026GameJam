f523b8ae31ad6b740e86cd175197c90866fef8b5
souya-teruya-k25f-ship-it <souya-teruya-k25f@stu.kbc.ac.jp>
2026-02-19 15:17:02 +0900

    当たり判定の計算処理

--- TeamA/Sources/GameObjects/Collision/Collision.cpp
+++ TeamA/Sources/GameObjects/Collision/Collision.cpp
@@ -12,5 +12,15 @@ const e_ObjectType Collision::GetObjectType() const
 
 const bool Collision::IsCircleColliding(const Vector2D& centerA, const Vector2D& centerB, const float& radiusB) const
 {
+    Vector2D diff = centerA - centerB; //２点間の距離ベクトルを求める
+    
+    float sum = m_radius + radiusB; //半径の合計
+    float c = diff.x + diff.y; //中心の距離
 
+//半径の合計の２乗<=距離の２乗の合計なら衝突
+    if (sum * sum <= c * c)
+    {
+        return true;
+    }
+    return false;
 }
